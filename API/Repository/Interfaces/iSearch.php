eac4f6deab29650d242c6d46f39ebf8e99f1f7e0
Ben Woodhead <bwoodhead@upei.ca>
2011-12-15 10:37:01 -0400

    Restored search

---
+++ API/Repository/Interfaces/iSearch.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Repository\Interfaces;
+
+interface iSearch 
+{
+}
+
+?>
---
+++ API/Repository/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+class Search 
+{
+  private $mSearch;
+  
+  public function __contructor( Graph\Interfaces\iSearch &$search )
+  {
+    $this->mSearch = $search;
+  }
+}
+
+?>
