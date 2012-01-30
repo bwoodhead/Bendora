53a82252690c3cb3519e8739c1763d4a154da779
Ben Woodhead <bwoodhead@upei.ca>
2012-01-30 10:49:46 -0400

    Formatting

--- Autoloader/example.php
+++ Autoloader/example.php
@@ -1,19 +1,22 @@
 <?php 
-spl_autoload_register(function ($className) { 
-    $possibilities = array( 
+spl_autoload_register(
+    function ($className) { 
+      $possibilities = array( 
         APPLICATION_PATH.'beans'.DIRECTORY_SEPARATOR.$className.'.php', 
         APPLICATION_PATH.'controllers'.DIRECTORY_SEPARATOR.$className.'.php', 
         APPLICATION_PATH.'libraries'.DIRECTORY_SEPARATOR.$className.'.php', 
         APPLICATION_PATH.'models'.DIRECTORY_SEPARATOR.$className.'.php', 
         APPLICATION_PATH.'views'.DIRECTORY_SEPARATOR.$className.'.php', 
         $className.'.php' 
-    ); 
-    foreach ($possibilities as $file) { 
+      ); 
+      foreach ($possibilities as $file) { 
         if (file_exists($file)) { 
             require_once($file); 
             return true; 
         } 
-    } 
-    return false; 
-}); 
+      } 
+      return false; 
+      
+      }
+    ); 
 ?>
